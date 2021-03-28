05d1827ba8f8b45305c7ced1fcee79c3263e4b99
iremakdgn <irem.akdgn.1996@gmail.com>
2021-03-28 16:09:16 +0300

    first commit

---
+++ src/views/AxiosPost.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="axios-post">
+    <form @submit.prevent="sendNewPost">
+      <label for="title">Başlık:</label>
+      <input name="title" type="text" v-model="newPost.title" />
+      <button type="submit" :disabled="inProgress">Kaydet</button>
+    </form>
+    <div v-if="sendNewPostResult">
+      Oluşan Post
+      <pre>{{ sendNewPostResult }}</pre>
+    </div>
+    <ul v-if="errors && errors.length">
+      <li v-for="(error, index) of errors" :key="index">{{ index + 1 }} - {{ error.message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      inProgress: false,
+      errors: [],
+      newPost: {
+        title: ""
+      },
+      sendNewPostResult: null
+    };
+  },
+  methods: {
+    sendNewPost() {
+      this.inProgress = true;
+      console.log(this.newPost);
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", this.newPost)
+        .then(response => (this.sendNewPostResult = response.data))
+        .catch(error => {
+          this.errors.push(error);
+        })
+        .finally(() => (this.inProgress = false));
+    }
+  }
+};
+</script>
